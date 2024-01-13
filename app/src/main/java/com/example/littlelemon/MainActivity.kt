7778f4345260dbcce7779f2503cadd049c527ba8
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-01-13 10:19:49 +0530

    minor changes

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,61 +3,80 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private val responseLiveData = MutableLiveData<String>()
-    private val httpClient = HttpClient(Android)
-    val glide = Glide.with(this)
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val menuItemsLiveData = MutableLiveData<List<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             LittleLemonTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
-                    val responseState = responseLiveData.observeAsState("").value
-
-                    Column {
-                        Button(
-                            onClick = {
-                                lifecycleScope.launch {
-                                    val response = fetchContent()
-                                    runOnUiThread {
-                                        responseLiveData.value = response
-                                    }
-                                }
-                            }
-                        ) {
-                            Text(text = "Download")
-                        }
-
-                        Text(text = responseState.toString())
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
                     }
                 }
             }
         }
     }
+}
 
-    private suspend fun fetchContent(): String {
-        return httpClient
-            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
-            .bodyAsText()
+@Composable
+fun MenuItems(
+    items: List<String> = emptyList(),
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        LazyColumn {
+            itemsIndexed(items) { _, item ->
+                MenuItemDetails(item)
+            }
+        }
     }
 }
+
+@Composable
+fun MenuItemDetails(menuItem: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(text = menuItem)
+    }
+}
+
