99b13c62fabb7d019348432b3b8dba0e2392b2e5
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-01-13 13:44:01 +0530

    minor changes

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
     private val responseLiveData = MutableLiveData<String>()
     private val httpClient = HttpClient(Android)
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
